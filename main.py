78f95c4fb7b05117fdb4e63037a5bfec88b7fc20
Raja Pandi <114086052+Rawwire@users.noreply.github.com>
2024-07-29 18:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import joblib 
 import os
+from pathlib import Path
 
-os.chdir("D:\Projects\Spam text Classifcation")
-rfc = joblib.load('models\\random_forest_spam_model.pkl')
-tfidf = joblib.load('models\\tfidf_vectorizer.pkl')
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+model_path = current_dir / "models" / "random_forest_spam_model.pkl"
+vectorizer_path = current_dir / "models" / "tfidf_vectorizer.pkl"
+
+
+rfc = joblib.load(model_path)
+tfidf = joblib.load(vectorizer_path)
+
 
 st.set_page_config(page_title="Spam Text Classification", layout="wide")
 st.title("Spam Text Classification")
 st.write("This project uses a Random Forest Classifier (Accuracy: 95%) to detect spam messages.")
 
+
 text_input = st.text_input("Enter the text to be checked:")
 if text_input:  
     x_transformed = tfidf.transform([text_input])
     prediction = rfc.predict(x_transformed.toarray())
-    st.write("Prediction:", prediction[0]) 
+    st.write("Prediction:", prediction[0])
